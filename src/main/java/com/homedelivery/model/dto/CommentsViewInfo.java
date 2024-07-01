929c46b44c68259aec5e7b28bec445cac16d96b2
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2024-07-02 00:22:40 +0300

    Slight changes in comments.html file and CommentController

--- src/main/java/com/homedelivery/model/dto/CommentsViewInfo.java
+++ src/main/java/com/homedelivery/model/dto/CommentsViewInfo.java
@@ -1,11 +1,15 @@
 package com.homedelivery.model.dto;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommentsViewInfo {
 
     List<CommentDetailsDTO> comments;
+
+    private double totalRating;
 
     public CommentsViewInfo() {
         this.comments = new ArrayList<>();
@@ -21,5 +25,21 @@ public class CommentsViewInfo {
 
     public void setComments(List<CommentDetailsDTO> comments) {
         this.comments = comments;
+    }
+
+    public double getTotalRating() throws ParseException {
+        Integer totalRating = this.comments.stream()
+                .map(CommentDetailsDTO::getRating)
+                .reduce(0, Integer::sum);
+
+        DecimalFormat df = new DecimalFormat("##.00");
+        String format = df.format((double) totalRating / this.comments.size());
+        double result = (double) df.parse(format);
+
+        return (this.comments.size() == 0) ? 0 : result;
+    }
+
+    public void setTotalRating(double totalRating) {
+        this.totalRating = totalRating;
     }
 }
